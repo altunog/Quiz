796a21fc581c19c742a9f6e244f192bb587aa1b2
Oğuz Kaan Altun <altunog@gmail.com>
2023-07-10 09:30:25 +0300

    add table view data source

---
+++ QuizApp/QuizAppTests/QuestionViewControllerTest.swift
@@ -0,0 +1,48 @@
+//
+//  QuestionViewController.swift
+//  QuizAppTests
+//
+//  Created by Oğuz Kaan Altun on 10.07.2023.
+//
+
+import Foundation
+import XCTest
+@testable import QuizApp
+
+class QuestionViewControllerTest: XCTestCase {
+    
+    func test_viewDidLoad_rendersQuestionHeaderText() {
+        let sut = QuestionViewController(question: "Q1", options: [])
+        
+        _ = sut.view
+        
+        XCTAssertEqual(sut.headerLabel.text, "Q1")
+    }
+    
+    func test_viewDidLoad_withNoOptions_rendersZeroOptions() {
+        let sut = QuestionViewController(question: "Q1", options: [])
+        
+        _ = sut.view
+        
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+    }
+    
+    func test_viewDidLoad_withOneOptions_rendersOneOption() {
+        let sut = QuestionViewController(question: "Q1", options: ["A1"])
+        
+        _ = sut.view
+        
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
+    }
+    
+    func test_viewDidLoad_withOneOptions_rendersOneOptionText() {
+        let sut = QuestionViewController(question: "Q1", options: ["A1"])
+        
+        _ = sut.view
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        let cell = sut.tableView.dataSource?.tableView(sut.tableView, cellForRowAt: indexPath)
+        
+        XCTAssertEqual(cell?.textLabel?.text, "A1")
+    }
+}
