59f84a4143030e84885638e244ceae59a3afc734
Oğuz Kaan Altun <altunog@gmail.com>
2023-07-12 22:59:13 +0300

    setup UI, test selection de selection of answers

--- QuizApp/QuizApp/QuestionViewController.swift
+++ QuizApp/QuizApp/QuestionViewController.swift
@@ -15,9 +15,9 @@ class QuestionViewController: UIViewController, UITableViewDataSource, UITableVi
     private var question: String = ""
     private var options: [String] = []
     private let reuseIdentifier = "Cell"
-    private var selection: ((String) -> Void)?
+    private var selection: (([String]) -> Void)?
     
-    convenience init(question: String, options: [String], selection: @escaping (String) -> Void) {
+    convenience init(question: String, options: [String], selection: @escaping ([String]) -> Void) {
         self.init()
         self.question = question
         self.options = options
@@ -41,7 +41,18 @@ class QuestionViewController: UIViewController, UITableViewDataSource, UITableVi
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selection?(options[indexPath.row])
+        selection?(selectedOptions(in: tableView))
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if tableView.allowsMultipleSelection {
+            selection?(selectedOptions(in: tableView))
+        }
+    }
+    
+    private func selectedOptions(in tableView: UITableView) -> [String] {
+        guard let indexPaths = tableView.indexPathsForSelectedRows else { return [] }
+        return indexPaths.map { options[$0.row] }
     }
     
     private func dequeueCell(in tableView: UITableView) -> UITableViewCell {
