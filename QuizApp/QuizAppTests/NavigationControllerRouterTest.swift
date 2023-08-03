4008331eaf2f53643444867377e8a1f4f3f1062e
Oğuz Kaan Altun <altunog@gmail.com>
2023-08-04 00:42:07 +0300

    add abstract factory and test concrete router class

--- QuizApp/QuizAppTests/NavigationControllerRouterTest.swift
+++ QuizApp/QuizAppTests/NavigationControllerRouterTest.swift
@@ -11,22 +11,48 @@ import XCTest
 
 class NavigationControllerRouterTest: XCTestCase {
     
-    func test_routeToQuestion_presentsQuestionController() {
-        let navigationController = UINavigationController()
-        let sut = NavigationControllerRouter(navigationController)
-        
-        sut.routeTo(question: "Q1", answerCallback: { _ in })
-        
-        XCTAssertEqual(navigationController.viewControllers.count, 1)
-    }
+    let navigationController = UINavigationController()
+    let factory = ViewControllerFactoryStub()
     
-    func test_routeToQuestionTwice_presentsQuestionController() {
-        let navigationController = UINavigationController()
-        let sut = NavigationControllerRouter(navigationController)
+    lazy var sut: NavigationControllerRouter = {
+        NavigationControllerRouter(navigationController, factory: factory)
+    }()
+    
+    func test_routeToSecondQuestion_showsQuestionControllers() {
+        let viewController = UIViewController()
+        let secondViewController = UIViewController()
+        factory.stub(question: "Q1", with: viewController)
+        factory.stub(question: "Q2", with: secondViewController)
         
         sut.routeTo(question: "Q1", answerCallback: { _ in })
         sut.routeTo(question: "Q2", answerCallback: { _ in })
         
         XCTAssertEqual(navigationController.viewControllers.count, 2)
+        XCTAssertEqual(navigationController.viewControllers.first, viewController)
+        XCTAssertEqual(navigationController.viewControllers.last, secondViewController)
+    }
+    
+    func test_routeToQuestion_showsQuestionControllerWithRightCallback() {
+        var callbackWasFired = false
+        
+        sut.routeTo(question: "Q1", answerCallback: { _ in  callbackWasFired = true })
+        factory.answerCallbacks["Q1"]!("an answer")
+        
+        XCTAssertTrue(callbackWasFired)
+    }
+    
+    class ViewControllerFactoryStub: ViewControllerFactory {
+        
+        var stubbedQuestions = [String: UIViewController]()
+        var answerCallbacks = [String: (String) -> Void]()
+        
+        func stub(question: String, with viewController: UIViewController) {
+            stubbedQuestions[question] = viewController
+        }
+        
+        func questionViewController(for question: String, answerCallback: @escaping (String) -> Void) -> UIViewController {
+            answerCallbacks[question] = answerCallback
+            return stubbedQuestions[question] ?? UIViewController()
+        }
     }
 }
