632c03cde102991a901a690264c82bdb628c552f
Oğuz Kaan Altun <altunog@gmail.com>
2023-08-05 12:03:57 +0300

    add unit tests to Question model for hashable

--- QuizApp/QuizApp/Question.swift
+++ QuizApp/QuizApp/Question.swift
@@ -11,6 +11,15 @@ enum Question<T: Hashable>: Hashable {
     case singleAnswer(T)
     case multipleAnswer(T)
     
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case .singleAnswer(let a):
+            hasher.combine(a)
+        case .multipleAnswer(let a):
+            hasher.combine(a)
+        }
+    }
+    
     static func ==(lhs: Question<T>, rhs: Question<T>) -> Bool {
         switch (lhs, rhs) {
         case (.singleAnswer(let a), .singleAnswer(let b)):
--- QuizApp/QuizAppTests/QuestionTest.swift
+++ QuizApp/QuizAppTests/QuestionTest.swift
@@ -10,21 +10,22 @@ import XCTest
 @testable import QuizApp
 
 class QuestionTest: XCTestCase {
-//    func test_hashValue_singleAnswer_returnsTypeHash() {
-//        let type = "a string"
-//
-//        let sut = Question.singleAnswer(type)
-//
-//        XCTAssertEqual(sut.hashValue, type.hashValue)
-//    }
-//
-//    func test_hashValue_multipleAnswer_returnsTypeHash() {
-//        let type = "a string"
-//
-//        let sut = Question.multipleAnswer(type)
-//
-//        XCTAssertEqual(sut.hashValue, type.hashValue)
-//    }
+    
+    func test_hashValue_singleAnswer_returnsTypeHash() {
+        let type = "a string"
+
+        let sut = Question.singleAnswer(type)
+
+        XCTAssertEqual(sut.hashValue, type.hashValue)
+    }
+
+    func test_hashValue_multipleAnswer_returnsTypeHash() {
+        let type = "a string"
+
+        let sut = Question.multipleAnswer(type)
+
+        XCTAssertEqual(sut.hashValue, type.hashValue)
+    }
     
     func test_equal_isEqual() {
         XCTAssertEqual(Question.singleAnswer("a string"), Question.singleAnswer("a string"))
