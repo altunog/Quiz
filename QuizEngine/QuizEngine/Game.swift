7e213d2df851ddc56e29044e741727977681d9b2
Oğuz Kaan Altun <altunog@gmail.com>
2023-08-13 14:57:31 +0300

    remove redundant type conformances

--- QuizEngine/QuizEngine/Game.swift
+++ QuizEngine/QuizEngine/Game.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public class Game<Question: Hashable, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
+public class Game<Question, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
     let flow: Flow<Question, Answer, R>
     
     init(flow: Flow<Question, Answer, R>) {
@@ -15,11 +15,7 @@ public class Game<Question: Hashable, Answer, R: Router> where R.Question == Que
     }
 }
 
-public func startGame<Question: Hashable, Answer: Equatable, R: Router>(
-    question: [Question],
-    router: R,
-    correctAnswers: [Question: Answer]
-) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
+public func startGame<Question, Answer: Equatable, R: Router>(question: [Question], router: R, correctAnswers: [Question: Answer]) -> Game<Question, Answer, R>{
     let flow = Flow(questions: question, router: router, scoring: { answers in
         return scoring(answers, correctAnswers: correctAnswers)
     })
@@ -27,7 +23,7 @@ public func startGame<Question: Hashable, Answer: Equatable, R: Router>(
     return Game(flow: flow)
 }
 
-private func scoring<Question: Hashable, Answer: Equatable> (_ answers: [Question: Answer], correctAnswers: [Question: Answer]) -> Int {
+private func scoring<Question, Answer: Equatable> (_ answers: [Question: Answer], correctAnswers: [Question: Answer]) -> Int {
     return answers.reduce(0) { score, tuple in
         return score + (correctAnswers[tuple.key] == tuple.value ? 1 : 0)
     }
