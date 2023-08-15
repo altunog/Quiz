81f3c7420bd115c4a6142bda40677a95074cab6a
Oğuz Kaan Altun <altunog@gmail.com>
2023-08-15 23:06:15 +0300

    remove Question Equatable custom implementation

--- QuizEngine/QuizEngine/Question.swift
+++ QuizEngine/QuizEngine/Question.swift
@@ -19,15 +19,4 @@ public enum Question<T: Hashable>: Hashable {
             hasher.combine(a)
         }
     }
-    
-    public static func == (lhs: Question<T>, rhs: Question<T>) -> Bool {
-        switch (lhs, rhs) {
-        case (.singleAnswer(let a), .singleAnswer(let b)):
-            return a == b
-        case (.multipleAnswer(let a), .multipleAnswer(let b)):
-            return a == b
-        default:
-            return false
-        }
-    }
 }
