48ae4ca522979e2fdfeeb222fa5c39fee441d986
Oğuz Kaan Altun <altunog@gmail.com>
2023-08-19 16:08:49 +0300

    remove Question's custom implementation of hash(into:) method, and make tests less fragile

--- QuizEngine/QuizEngine/Question.swift
+++ QuizEngine/QuizEngine/Question.swift
@@ -10,13 +10,4 @@ import Foundation
 public enum Question<T: Hashable>: Hashable {
     case singleAnswer(T)
     case multipleAnswer(T)
-    
-    public func hash(into hasher: inout Hasher) {
-        switch self {
-        case .singleAnswer(let a):
-            hasher.combine(a)
-        case .multipleAnswer(let a):
-            hasher.combine(a)
-        }
-    }
 }
