b1dceaf604ad8e9e8653e1172cf9de5f663b5562
Oğuz Kaan Altun <altunog@gmail.com>
2023-08-30 19:04:57 +0300

    move Router to deprecated components' file, remove its file

--- QuizEngine/QuizEngine/DeprecatedGame.swift
+++ QuizEngine/QuizEngine/DeprecatedGame.swift
@@ -8,6 +8,15 @@
 import Foundation
 
 @available(*, deprecated)
+public protocol Router {
+    associatedtype Question: Hashable
+    associatedtype Answer
+    
+    func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
+    func routeTo(result: Result<Question, Answer>)
+}
+
+@available(*, deprecated)
 public class Game<Question, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
     let flow: Any
     
