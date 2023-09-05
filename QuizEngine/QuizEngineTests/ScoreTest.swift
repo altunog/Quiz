7e2c2843f17f8c9aeec8309ea0d1636b1948d71b
Oğuz Kaan Altun <altunog@gmail.com>
2023-09-05 21:09:37 +0300

    score zero for wrong answer

--- QuizEngine/QuizEngineTests/ScoreTest.swift
+++ QuizEngine/QuizEngineTests/ScoreTest.swift
@@ -9,12 +9,17 @@ import Foundation
 import XCTest
 
 class ScoreTest: XCTestCase {
+    
     func test_noAnswers_scoresZero() {
-        XCTAssertEqual(BasicScore.score(for: []), 0)
+        XCTAssertEqual(BasicScore.score(for: [], comparingTo: []), 0)
+    }
+    
+    func test_oneWrongAnswer_scoresZero() {
+        XCTAssertEqual(BasicScore.score(for: ["wrong"], comparingTo: ["correct"]), 0)
     }
     
     private class BasicScore {
-        static func score(for: [Any]) -> Int {
+        static func score(for: [Any], comparingTo: [Any]) -> Int {
             return 0
         }
     }
