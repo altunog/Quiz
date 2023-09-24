5ec0fb69b2ae6ed989ff87121677764a56f3627c
Oğuz Kaan Altun <altunog@gmail.com>
2023-09-24 14:15:45 +0300

    capture answer completions in ordered form

--- QuizEngine/QuizEngineTests/FlowTest.swift
+++ QuizEngine/QuizEngineTests/FlowTest.swift
@@ -48,8 +48,8 @@ class FlowTest: XCTestCase {
         let sut = makeSUT(questions: ["Q1", "Q2", "Q3"])
         sut.start()
         
-        delegate.answerCompletion("A1")
-        delegate.answerCompletion("A2")
+        delegate.answerCompletions[0]("A1")
+        delegate.answerCompletions[1]("A2")
 
         XCTAssertEqual(delegate.questionsAsked, ["Q1", "Q2", "Q3"])
     }
@@ -58,7 +58,7 @@ class FlowTest: XCTestCase {
         let sut = makeSUT(questions: ["Q1"])
         sut.start()
         
-        delegate.answerCompletion("A1")
+        delegate.answerCompletions[0]("A1")
 
         XCTAssertEqual(delegate.questionsAsked, ["Q1"])
     }
@@ -80,7 +80,7 @@ class FlowTest: XCTestCase {
         let sut = makeSUT(questions: ["Q1", "Q2"])
         sut.start()
         
-        delegate.answerCompletion("A1")
+        delegate.answerCompletions[0]("A1")
         
         XCTAssertTrue(delegate.completedQuizzes.isEmpty)
     }
@@ -89,8 +89,8 @@ class FlowTest: XCTestCase {
         let sut = makeSUT(questions: ["Q1", "Q2"])
         sut.start()
         
-        delegate.answerCompletion("A1")
-        delegate.answerCompletion("A2")
+        delegate.answerCompletions[0]("A1")
+        delegate.answerCompletions[1]("A2")
         
         XCTAssertEqual(delegate.completedQuizzes.count, 1)
         assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
@@ -120,14 +120,14 @@ class FlowTest: XCTestCase {
     
     private class DelegateSpy: QuizDelegate {
         var questionsAsked: [String] = []
+        var answerCompletions: [(String) -> Void] = []
+        
         var handledResult: Result<String, String>? = nil
         var completedQuizzes: [[(String, String)]] = []
         
-        var answerCompletion: ((String) -> Void) = { _ in }
-        
         func answer(for question: String, completion: @escaping (String) -> Void) {
             questionsAsked.append(question)
-            self.answerCompletion = completion
+            answerCompletions.append(completion)
         }
         
         func didCompleteQuiz(withAnswers answers: [(question: String, answer: String)]) {
