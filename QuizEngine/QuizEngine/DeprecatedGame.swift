29cac2f8dab5f8da9ce5d7f92cdd25dc16d08924
Oğuz Kaan Altun <altunog@gmail.com>
2023-09-24 14:32:54 +0300

    deprecate the Result type

--- QuizEngine/QuizEngine/DeprecatedGame.swift
+++ QuizEngine/QuizEngine/DeprecatedGame.swift
@@ -17,6 +17,12 @@ public protocol Router {
 }
 
 @available(*, deprecated)
+public struct Result<Question: Hashable, Answer> {
+    public let answers: [Question: Answer]
+    public let score: Int
+}
+
+@available(*, deprecated)
 public class Game<Question, Answer, R: Router> {
     let flow: Any
     
