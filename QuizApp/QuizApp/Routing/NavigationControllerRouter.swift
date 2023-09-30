6cd926746c2d033dba021e1d554e22d927326be7
Oğuz Kaan Altun <altunog@gmail.com>
2023-09-30 22:46:43 +0300

    fix tuple name mismatch in the QuizDelegate implementtion

--- QuizApp/QuizApp/Routing/NavigationControllerRouter.swift
+++ QuizApp/QuizApp/Routing/NavigationControllerRouter.swift
@@ -37,8 +37,8 @@ class NavigationControllerRouter: Router {
         answer(for: question, completion: answerCallback)
     }
     
-    func didCompleteQuiz(withAnswers answers: [(question: QuizEngine.Question<String>, answers: [String])]) {
-        show(factory.resultViewController(for: answers))
+    func didCompleteQuiz(withAnswers answers: [(question: QuizEngine.Question<String>, answer: [String])]) {
+        show(factory.resultViewController(for: answers.map { ($0.question, $0.answer) }))
     }
     
     func routeTo(result: Result<Question<String>, [String]>) {
