06d8ba2aa2e8f11f65077eeba8bc83c65f90b9cb
Oğuz Kaan Altun <altunog@gmail.com>
2023-09-30 15:55:08 +0300

    remove the deprecated makeSUT function

--- QuizApp/QuizAppTests/Main/iOSViewControllerFactoryTest.swift
+++ QuizApp/QuizAppTests/Main/iOSViewControllerFactoryTest.swift
@@ -67,17 +67,17 @@ class iOSViewControllerFactoryTest: XCTestCase {
     
     // MARK: Helpers
     
-    func makeSUT(options: [Question<String> : [String]] = [:], correctAnswers: [Question<String> : [String]] = [:]) -> iOSViewControllerFactory {
-        let sut = iOSViewControllerFactory(questions: [singleAnswerQuestion, multipleAnswerQuestion], options: options, correctAnswers: correctAnswers)
-        return sut
-    }
-    
     func makeSUT(options: [Question<String> : [String]] = [:], correctAnswers: [(Question<String>, [String])] = []) -> iOSViewControllerFactory {
         return iOSViewControllerFactory(options: options, correctAnswers: correctAnswers)
     }
      
     func makeQuestionController(question: Question<String> = Question.singleAnswer("")) -> QuestionViewController {
-        return makeSUT(options: [question: options], correctAnswers: [:]).questionViewController(for: question, answerCallback: {_ in} ) as! QuestionViewController
+        let sut = makeSUT(
+            options: [question: options],
+            correctAnswers: [(singleAnswerQuestion, []), (multipleAnswerQuestion, [])]
+        )
+        
+        return sut.questionViewController(for: question, answerCallback: {_ in} ) as! QuestionViewController
     }
     
     func makeResults() -> (controller: ResultsViewController, presenter: ResultsPresenter) {
