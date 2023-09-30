163df92dbd9262c92e37cafffc9e3746137597ea
Oğuz Kaan Altun <altunog@gmail.com>
2023-09-30 12:19:02 +0300

    Update iOSViewControllerFactory.swift
    update the factory implementation to use new ResultPresenter initializer

--- QuizApp/QuizApp/Main/iOSViewControllerFactory.swift
+++ QuizApp/QuizApp/Main/iOSViewControllerFactory.swift
@@ -45,7 +45,12 @@ class iOSViewControllerFactory: ViewControllerFactory {
     }
     
     func resultViewController(for result: Result<Question<String>, [String]>) -> UIViewController {
-        let presenter = ResultsPresenter(result: result, questions: questions, correctAnswers: correctAnswers)
+        let presenter = ResultsPresenter(userAnswers: questions.map { question in
+            (question, result.answers[question]!)
+        }, correctAnswers: questions.map { question in
+            (question, correctAnswers[question]!)
+        }, scorer: { _, _ in result.score }
+        )
         let controller = ResultsViewController(summary: presenter.summary, answers: presenter.presentableAnswers)
         controller.title = presenter.title
         return controller
