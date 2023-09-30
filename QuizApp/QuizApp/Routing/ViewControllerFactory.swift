b0b6213499f7b9973a6360e3ecb3c1771e140901
Oğuz Kaan Altun <altunog@gmail.com>
2023-09-30 16:15:38 +0300

    introduce the new factory method to create result viewcontrollers

--- QuizApp/QuizApp/Routing/ViewControllerFactory.swift
+++ QuizApp/QuizApp/Routing/ViewControllerFactory.swift
@@ -9,6 +9,17 @@ import UIKit
 import QuizEngine
 
 protocol ViewControllerFactory {
+    typealias Answers = [(question: Question<String>, answers: [String])]
+    
     func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController
+    
+    func resultViewController(for userAnswers: Answers) -> UIViewController
+    
     func resultViewController(for result: Result<Question<String>, [String]>) -> UIViewController
+}
+
+extension ViewControllerFactory {
+    func resultViewController(for userAnswers: Answers) -> UIViewController {
+        return UIViewController()
+    }
 }
