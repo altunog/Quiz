5af49fad20c10b555b63f6583861003abc5b9878
Oğuz Kaan Altun <altunog@gmail.com>
2023-09-30 23:10:35 +0300

    remove deprecated method

--- QuizApp/QuizApp/Routing/ViewControllerFactory.swift
+++ QuizApp/QuizApp/Routing/ViewControllerFactory.swift
@@ -14,6 +14,4 @@ protocol ViewControllerFactory {
     func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController
     
     func resultViewController(for userAnswers: Answers) -> UIViewController
-    
-    func resultViewController(for result: Result<Question<String>, [String]>) -> UIViewController
 }
