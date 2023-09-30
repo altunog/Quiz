5cd858833b6d4d06bc303b4275a67cb4bfc03e6b
Oğuz Kaan Altun <altunog@gmail.com>
2023-09-30 12:01:35 +0300

    Update ResultsPresenterTest.swift
    migrate the summary test to the new API

--- QuizApp/QuizAppTests/Presentation/Result/ResultsPresenterTest.swift
+++ QuizApp/QuizAppTests/Presentation/Result/ResultsPresenterTest.swift
@@ -20,11 +20,10 @@ class ResultsPresenterTest: XCTestCase {
     }
     
     func test_summary_withTwoQuestionsAndScoreOne_returnsSummary() {
-        let answers = [singleAnswerQuestion: ["A1"], multipleAnswerQuestion: ["A1", "A2"]]
-        let correctAnswers = [singleAnswerQuestion: ["A1"], multipleAnswerQuestion: ["A2"]]
-        let result = Result.make(answers: answers, score: 1)
+        let userAnswers = [(singleAnswerQuestion, ["A1"]), (multipleAnswerQuestion, ["A1", "A2"])]
+        let correctAnswers = [(singleAnswerQuestion, ["A1"]), (multipleAnswerQuestion, ["A2"])]
         
-        let sut = ResultsPresenter(result: result, questions: [singleAnswerQuestion, multipleAnswerQuestion], correctAnswers: correctAnswers)
+        let sut = makeSUT(userAnswers: userAnswers, correctAnswers: correctAnswers, score: 1)
         
         XCTAssertEqual(sut.summary, "You got 1/2 correct")
     }
@@ -81,7 +80,11 @@ class ResultsPresenterTest: XCTestCase {
     
     // MARK: Helpers
     
-    private func makeSUT() -> ResultsPresenter {
-        return ResultsPresenter(userAnswers: [], correctAnswers: [], scorer: { _, _ in 0 })
+    private func makeSUT(
+        userAnswers: ResultsPresenter.Answers = [],
+        correctAnswers: ResultsPresenter.Answers = [],
+        score: Int = 0
+    ) -> ResultsPresenter {
+        return ResultsPresenter(userAnswers: userAnswers, correctAnswers: correctAnswers, scorer: { _, _ in score })
     }
 }
