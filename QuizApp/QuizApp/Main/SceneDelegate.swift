68d4771751cb940a8056b8d80fdc5a76a11bf958
Oğuz Kaan Altun <altunog@gmail.com>
2023-09-30 23:03:32 +0300

    migrate the SceneDelegate to the new Quiz APIs

--- QuizApp/QuizApp/Main/SceneDelegate.swift
+++ QuizApp/QuizApp/Main/SceneDelegate.swift
@@ -11,12 +11,13 @@ import QuizEngine
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    
-    var game: Game<Question<String>, [String], NavigationControllerRouter>?
+    var game: Quiz?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
         let question1 = Question.singleAnswer("What's Mike's nationality?")
         let question2 = Question.multipleAnswer("What's Caio's nationality?")
+        let questions = [question1, question2]
         
         let option1 = "Canadian"
         let option2 = "American"
@@ -28,12 +29,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let option6 = "Brazillian"
         let options2 = [option4, option5, option6]
         
+        let options = [question1: options1, question2: options2]
+        let correctAnswers = [(question1, [option3]), (question2, [option4, option6])]
+        let factory = iOSViewControllerFactory(options: options, correctAnswers: correctAnswers)
+        
         let navigationController = UINavigationController()
-        let correctAnswers = [question1: [option3], question2: [option4, option6]]
-        let factory = iOSViewControllerFactory(
-            options: [question1: options1, question2: options2],
-            correctAnswers: [(question1, [option3]), (question2, [option4, option6])]
-        )
         let router = NavigationControllerRouter(navigationController, factory: factory)
         
         guard let scene = (scene as? UIWindowScene) else { return }
@@ -41,37 +41,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
-        game = startGame(question: [question1, question2], router: router, correctAnswers: correctAnswers)
+        game = Quiz.start(question: questions, delegate: router)
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
